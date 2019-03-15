2984e9e232980a2386d8fefa9e287bd94fd43784
城西システム 水野 <jos.s.mizuno@gmail.com>
2019-03-15 16:25:35 +0900

    [add] JSONPをJSONに変換するUtilityを実装。

---
+++ src/laravel/app/Libs/Utility/Json.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Libs\Utility;
+
+class Json {
+
+	/**
+	 * JSONPからJSONへ変換する
+	 * @param $jsonp
+	 * @return string|string[]|null
+	 */
+	public static function convertJsonpToJson($jsonp) {
+		return preg_replace('/.+?({.+}).+/', "$1", $jsonp);
+	}
+}
