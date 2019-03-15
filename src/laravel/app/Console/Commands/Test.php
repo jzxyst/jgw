20c5fb570cf2c9f81ef2ab6240d9eb3c58870244
城西システム 水野 <jos.s.mizuno@gmail.com>
2019-03-15 16:28:01 +0900

    [add] test実行用のコマンドを追加。

---
+++ src/laravel/app/Console/Commands/Test.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+
+class Test extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'test';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'test';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+    public function handle()
+    {
+        //
+        $user = new \App\Libs\Seeder\User();
+        $users = $user->getUser(200);
+        print_r($users);
+    }
+}
