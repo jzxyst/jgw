970abeff668c78237633958c819ce4ff25e54f49
城西システム 水野 <jos.s.mizuno@gmail.com>
2019-03-15 18:07:10 +0900

    [add] user table seederのベースを実装。

---
+++ src/laravel/database/seeds/UserTableSeeder.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class UserTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //
+        $user = new \App\Libs\Seeder\User();
+        $users = $user->getUser(100);
+        foreach ($users as $user) {
+            list($name1, $name2) = explode(' ', $user[0]);
+            list($name1_kana, $name2_kana) = explode(' ', $user[1]);
+
+            $user_model = \App\Model\User::create([
+                'mail' => '',
+                'name1' => $name1,
+                'name2' => $name2,
+            ]);
+        }
+    }
+}
