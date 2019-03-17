ea0ccb9d6a143350563e167a8ae0ad4e39b7a423
Shohei Mizuno <jzxyst@gmail.com>
2019-03-17 23:04:47 +0900

    [update] users.sex_idにリレーションを実装。

--- src/laravel/app/Model/User.php
+++ src/laravel/app/Model/User.php
@@ -29,4 +29,9 @@ class User extends Authenticatable
     protected $hidden = [
         'user_id', 'password', 'salt',
     ];
+
+    public function sex()
+    {
+        return $this->hasOne('\App\Model\Sex', 'sex_id', 'sex_id');
+    }
 }
