2bc4820204e3453e723cccb3a58463c654e5a47f
Shohei Mizuno <jzxyst@gmail.com>
2019-03-25 00:21:27 +0900

    add User model.

---
+++ src/laravel/app/Model/User.php
@@ -0,0 +1,12 @@
+<?php
+namespace App\Model;
+
+class User extends ModelBase
+{
+    protected static $className = 'User';
+
+//    public static function create(array $data)
+//    {
+//        print self::class;
+//    }
+}
