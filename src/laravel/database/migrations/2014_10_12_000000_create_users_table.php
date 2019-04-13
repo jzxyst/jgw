f654c60b45bf79d9473f5bba26ec4dd09fa5effe
Shohei Mizuno <jzxyst@gmail.com>
2019-04-13 19:55:22 +0900

    [update] usersテーブルを論理削除可能に変更。

--- src/laravel/database/migrations/2014_10_12_000000_create_users_table.php
+++ src/laravel/database/migrations/2014_10_12_000000_create_users_table.php
@@ -29,6 +29,7 @@ class CreateUsersTable extends Migration
             $table->timestamp('email_verified_at')->nullable();
             $table->timestamp('created_at')->default(DB::raw('CURRENT_TIMESTAMP'));
             $table->timestamp('updated_at')->default(DB::raw('CURRENT_TIMESTAMP on update CURRENT_TIMESTAMP'));
+            $table->softDeletes();
         });
     }
 
