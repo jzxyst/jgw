0af53754a35c50c8a6b35c3a252af57bd14425cc
Shohei Mizuno <jzxyst@gmail.com>
2019-04-16 23:53:24 +0900

    [update] enums.sexを多言語化。

--- src/laravel/app/Enums/Sex.php
+++ src/laravel/app/Enums/Sex.php
@@ -3,8 +3,9 @@
 namespace App\Enums;
 
 use BenSampo\Enum\Enum;
+use BenSampo\Enum\Contracts\LocalizedEnum;
 
-final class Sex extends Enum
+final class Sex extends Enum implements LocalizedEnum
 {
     const NOT_KNOWN = 0;
     const MALE = 1;
