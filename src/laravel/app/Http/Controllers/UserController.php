1a0c55dda0fc3fc26813bcb9368987e6c7d3da46
Shohei Mizuno <jzxyst@gmail.com>
2019-04-20 00:31:42 +0900

    [clean] 不要なコードを削除。

--- src/laravel/app/Http/Controllers/UserController.php
+++ src/laravel/app/Http/Controllers/UserController.php
@@ -16,7 +16,7 @@ class UserController extends Controller
     public function index()
     {
         return response()->json(
-            \App\Orm\User::paginate()
+            User::paginate()
         );
     }
 
