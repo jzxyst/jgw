509fdf455aa785e17c6438353147e949035977f0
Shohei Mizuno <jzxyst@gmail.com>
2019-04-22 23:36:17 +0900

    [update] Model Baseにupdateメソッドを追加。

--- src/laravel/app/Model/ModelBase.php
+++ src/laravel/app/Model/ModelBase.php
@@ -5,12 +5,49 @@ class ModelBase
 {
     protected static $className;
 
+    protected static $primaryKey;
+
+    /**
+     * @param array $values
+     * @return mixed
+     */
     public static function create(array $values)
     {
         // orm instance
         $namespace = config('model.orm_namespace');
         $ormName = $namespace . '\\' . static::$className;
         $orm = new $ormName();
+
+        // add to attributes
+        foreach ($values as $key => $value) {
+            $orm->{$key} = $value;
+        }
+        $orm->save();
+
+        return $orm;
+    }
+
+    /**
+     * @param array $values
+     * @param null $primaryKey
+     * @return null
+     */
+    public static function update(array $values, $primaryKey = null)
+    {
+        // Primary key not set.
+        if (isset($values[self::$primaryKey]) === false and isset($primaryKey) === false) {
+            return null;
+        }
+
+        // Define primary key in array.
+        if (isset($values[self::$primaryKey]) and isset($primaryKey) === false) {
+            $primaryKey = $values[self::$primaryKey];
+        }
+
+        // orm instance
+        $namespace = config('model.orm_namespace');
+        $ormName = $namespace . '\\' . static::$className;
+        $orm = $ormName::find($primaryKey);
 
         // add to attributes
         foreach ($values as $key => $value) {
