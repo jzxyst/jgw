43808892b89bc367b2dd61d409bfbb3cd7ec5fd5
Shohei Mizuno <jzxyst@gmail.com>
2019-05-10 20:06:04 +0900

    [add] structuresテーブルを実装。

---
+++ src/laravel/database/migrations/1970_01_01_000000_create_structures_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateStructuresTable extends Migration
+{
+    private $table_name = 'structures';
+
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create($this->table_name, function (Blueprint $table) {
+            $table->increments('structure_id')->comment('構造ID');
+            $table->unsignedInteger('parent_structure_id')->comment('親構造ID');
+            $table->string('structure_name')->comment('構造名');
+            $table->timestamp('created_at')->default(DB::raw('CURRENT_TIMESTAMP'));
+            $table->timestamp('updated_at')->default(DB::raw('CURRENT_TIMESTAMP on update CURRENT_TIMESTAMP'));
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists($this->table_name);
+    }
+}
