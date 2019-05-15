81f95d5142173e5778387f58721a6b5ae74d697f
Shohei Mizuno <jzxyst@gmail.com>
2019-05-15 11:42:16 +0900

    [change] migrationファイルのprefixを統一。

--- src/laravel/database/migrations/1970_01_01_000000_create_positions_table.php
+++ src/laravel/database/migrations/1970_01_01_000000_create_positions_table.php
@@ -4,9 +4,9 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class PositionGroupsTable extends Migration
+class CreatePositionsTable extends Migration
 {
-    private $table_name = 'position_groups';
+    private $table_name = 'positions';
 
     /**
      * Run the migrations.
@@ -16,8 +16,9 @@ class PositionGroupsTable extends Migration
     public function up()
     {
         Schema::create($this->table_name, function (Blueprint $table) {
-            $table->increments('position_group_id')->comment('役職グループID');
-            $table->string('position_group_name')->comment('役職グループ名');
+            $table->increments('position_id')->comment('役職ID');
+            $table->unsignedInteger('position_group_id')->comment('役職グループID');
+            $table->string('position_name')->comment('役職名');
             $table->integer('sort_number')->comment('ソート順序');
             $table->timestamp('created_at')->default(DB::raw('CURRENT_TIMESTAMP'));
             $table->timestamp('updated_at')->default(DB::raw('CURRENT_TIMESTAMP on update CURRENT_TIMESTAMP'));
--- src/laravel/database/migrations/1970_01_01_000000_create_sexes_table.php
+++ src/laravel/database/migrations/1970_01_01_000000_create_sexes_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class SexesTable extends Migration
+class CreateSexesTable extends Migration
 {
     private $table_name = 'sexes';
 
