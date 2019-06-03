a89fb3f63f155de7e9a1c80e489ad529093efdcb
Shohei Mizuno <jzxyst@gmail.com>
2019-06-03 16:50:54 +0900

    [add] 認可のベースコードを実装。

--- src/laravel/app/Http/Controllers/UserController.php
+++ src/laravel/app/Http/Controllers/UserController.php
@@ -9,6 +9,11 @@ use App\Http\Requests\StoreUserRequest;
 
 class UserController extends Controller
 {
+    public function __construct()
+    {
+        $this->authorizeResource(User::class, 'user');
+    }
+
     /**
      * Display a listing of the resource.
      *
