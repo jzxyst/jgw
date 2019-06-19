631f2c64deb4f784c8cee90084c007a44d619dc8
Shohei Mizuno <jzxyst@gmail.com>
2019-06-19 16:26:11 +0900

    [clean] add JSDoc.

--- src/laravel/resources/js/store/auth.js
+++ src/laravel/resources/js/store/auth.js
@@ -13,15 +13,35 @@ const mutations = {
 };
 
 const actions = {
+
+    /**
+     * Call sign API.
+     * @param context
+     * @returns {Promise<void>}
+     */
     async sign (context) {
         const response = await axios.get('/api/sign');
         const user = response.data || null;
         context.commit('setUser', user)
     },
+
+    /**
+     * Call sign in API.
+     * @param context
+     * @param data
+     * @returns {Promise<void>}
+     */
     async signIn (context, data) {
         const response = await axios.post('/api/signin', data);
         context.commit('setUser', response.data);
     },
+
+    /**
+     * Call sign out API.
+     * @param context
+     * @param data
+     * @returns {Promise<void>}
+     */
     async signOut (context, data) {
         const response = await axios.post('/api/signout', data);
         context.commit('setUser', null);
