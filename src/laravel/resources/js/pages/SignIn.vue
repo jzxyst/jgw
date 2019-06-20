d38d6342f7b8309e4ad751794af9839c30fc0d0c
Shohei Mizuno <jzxyst@gmail.com>
2019-06-20 19:11:16 +0900

    [update] Sign inフォームでのエラー処理を追加。

--- src/laravel/resources/js/pages/SignIn.vue
+++ src/laravel/resources/js/pages/SignIn.vue
@@ -9,14 +9,15 @@
                                 <v-toolbar-title>Sign In</v-toolbar-title>
                                 <v-spacer></v-spacer>
                             </v-toolbar>
+                            <v-progress-linear class="ma-0" :indeterminate="isRequesting" :active="isRequesting"></v-progress-linear>
                             <v-form @submit.prevent="signIn">
                                 <v-card-text>
-                                    <v-text-field name="email" label="Email" type="email" prepend-icon="person" autofocus v-model="signInForm.email"></v-text-field>
-                                    <v-text-field name="password" label="Password" type="password" prepend-icon="lock" v-model="signInForm.password"></v-text-field>
+                                    <v-text-field name="email" label="Email" type="email" prepend-icon="person" autofocus v-model="values.signInForm.email" :error="!!errors.signInForm.email" :error-messages="errors.signInForm.email" :disabled="isRequesting"></v-text-field>
+                                    <v-text-field name="password" label="Password" type="password" prepend-icon="lock" v-model="values.signInForm.password" :error="!!errors.signInForm.password" :error-messages="errors.signInForm.password" :disabled="isRequesting"></v-text-field>
                                 </v-card-text>
                                 <v-card-actions>
                                     <v-spacer></v-spacer>
-                                    <v-btn color="primary" type="submit" :disabled="!signInForm.email || !signInForm.password">Sign In</v-btn>
+                                    <v-btn color="primary" type="submit" :disabled="isAvailableSubmit">Sign In</v-btn>
                                 </v-card-actions>
                             </v-form>
                         </v-card>
@@ -32,22 +33,60 @@
         name: "SignIn",
         data () {
             return {
-                signInForm: {
-                    email: '',
-                    password: ''
+                values: {
+                    signInForm: {
+                        email: '',
+                        password: ''
+                    },
                 },
+                errors: {
+                    signInForm: {
+                        email: null,
+                        password: null
+                    },
+                },
+                isRequesting: false
             }
+        },
+        computed: {
+            isAvailableSubmit () {
+                return !this.values.signInForm.email || !this.values.signInForm.password || this.isRequesting;
+            },
         },
         methods: {
             async signIn () {
 
+                this.clearErrors();
+
                 // Call sign in api.
-                await this.$store.dispatch('auth/signIn', this.signInForm);
+                this.startRequest();
+                await this.$store.dispatch('auth/signIn', this.values.signInForm);
+                this.endRequest();
 
                 // Sign in succeeded.
                 if (this.$store.getters['auth/isSignedIn']) {
                     this.$router.push(this.$route.query.redirect);
+                } else {
+                    this.assignErrors();
                 }
+            },
+
+            startRequest () {
+                this.isRequesting = true;
+            },
+
+            endRequest () {
+                this.isRequesting = false;
+            },
+
+            clearErrors () {
+                for (const key in this.errors.signInForm) {
+                    this.errors.signInForm[key] = null;
+                }
+            },
+
+            assignErrors () {
+                this.errors.signInForm = this.$store.state.auth.api.errors;
             }
         }
     }
