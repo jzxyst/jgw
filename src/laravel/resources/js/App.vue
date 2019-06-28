5695e96ddd55dd9de04e4570d18459c896b7df99
Shohei Mizuno <jzxyst@gmail.com>
2019-06-28 17:41:10 +0900

    [fix] Component名から".vue"を削除。

--- src/laravel/resources/js/App.vue
+++ src/laravel/resources/js/App.vue
@@ -4,7 +4,7 @@
 
 <script>
     export default {
-        name: "App.vue"
+        name: "App"
     }
 </script>
 
--- src/laravel/resources/js/pages/Dashboard.vue
+++ src/laravel/resources/js/pages/Dashboard.vue
@@ -4,7 +4,7 @@
 
 <script>
     export default {
-        name: "Dashboard.vue"
+        name: "Dashboard"
     }
 </script>
 
