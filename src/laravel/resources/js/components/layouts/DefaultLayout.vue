79f1f6d4fe49b290cf6eae9e4fc5a17b7fafa457
Shohei Mizuno <jzxyst@gmail.com>
2019-07-02 14:36:27 +0900

    [update] サイドナビゲーションのベースを実装。

--- src/laravel/resources/js/components/layouts/DefaultLayout.vue
+++ src/laravel/resources/js/components/layouts/DefaultLayout.vue
@@ -1,7 +1,37 @@
 <template>
     <v-app>
-        <v-navigation-drawer app></v-navigation-drawer>
-        <v-toolbar app></v-toolbar>
+        <v-navigation-drawer app clipped v-model="drawer">
+            <v-list dense>
+                <v-list-tile to="/dashboard">
+                    <v-list-tile-action>
+                        <v-icon>dashboard</v-icon>
+                    </v-list-tile-action>
+                    <v-list-tile-content>
+                        <v-list-tile-title>Dashboard</v-list-tile-title>
+                    </v-list-tile-content>
+                </v-list-tile>
+                <v-list-tile to="/userlist">
+                    <v-list-tile-action>
+                        <v-icon>account_box</v-icon>
+                    </v-list-tile-action>
+                    <v-list-tile-content>
+                        <v-list-tile-title>UserList</v-list-tile-title>
+                    </v-list-tile-content>
+                </v-list-tile>
+                <v-list-tile to="/settings">
+                    <v-list-tile-action>
+                        <v-icon>settings</v-icon>
+                    </v-list-tile-action>
+                    <v-list-tile-content>
+                        <v-list-tile-title>Settings</v-list-tile-title>
+                    </v-list-tile-content>
+                </v-list-tile>
+            </v-list>
+        </v-navigation-drawer>
+        <v-toolbar app fixed clipped-left dark color="primary">
+            <v-toolbar-side-icon @click.stop="drawer = !drawer"></v-toolbar-side-icon>
+            <v-toolbar-title>{{ this.$store.state.global.app.name }}</v-toolbar-title>
+        </v-toolbar>
         <v-content>
             <transition name="fade" mode="out-in">
                 <router-view />
@@ -12,7 +42,12 @@
 
 <script>
     export default {
-        name: "DefaultLayout"
+        name: "DefaultLayout",
+        data () {
+            return {
+                drawer: true
+            }
+        }
     }
 </script>
 
