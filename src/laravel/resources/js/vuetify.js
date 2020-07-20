266d6454e3ad64df55d51c5302ae878a09736580
Shohei Mizuno <jzxyst@gmail.com>
2020-07-20 23:55:19 +0900

    [add] Material Design Iconsを有効化。
    https://vuetifyjs.com/ja/customization/icons/

--- src/laravel/resources/js/vuetify.js
+++ src/laravel/resources/js/vuetify.js
@@ -4,6 +4,9 @@ import Vuetify from 'vuetify';
 Vue.use(Vuetify);
 
 const vuetify = new Vuetify({
+    icons: {
+        iconfont: 'mdiSvg', // 'mdi' || 'mdiSvg' || 'md' || 'fa' || 'fa4' || 'faSvg'
+    },
 });
 
 export default vuetify;
